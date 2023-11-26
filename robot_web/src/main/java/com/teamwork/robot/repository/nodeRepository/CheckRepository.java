32cdc453d29acf354d6783b11ed1fe6caf4da631
TanYYin <144016761+TanYYin@users.noreply.github.com>
2023-11-26 13:30:37 +0800

    Update CheckRepository.java

--- robot_web/src/main/java/com/teamwork/robot/repository/nodeRepository/CheckRepository.java
+++ robot_web/src/main/java/com/teamwork/robot/repository/nodeRepository/CheckRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 /**
-<<<<<<< dev-Xinjie
  * @author Crystry
  * @date 2021/11/22 21:42
  */
