eeab5b292d94e58bd38a61f7f0e4caad2cad3ca0
TanYYin <144016761+TanYYin@users.noreply.github.com>
2023-11-26 13:16:23 +0800

    Update DepartmentRepository.java

--- robot_web/src/main/java/com/teamwork/robot/repository/nodeRepository/DepartmentRepository.java
+++ robot_web/src/main/java/com/teamwork/robot/repository/nodeRepository/DepartmentRepository.java
@@ -5,12 +5,9 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Created with IntelliJ IDEA.
- *
- * @Author: jzy
- * @Date: 2023/11/23/22:43
- * @Description:
- */
+<<<<<<< dev-Xinjie
+ * @author Crystry
+ * @date 2021/11/22 21:42
 @Repository
 public interface DepartmentRepository extends Neo4jRepository<Department,Long> {
 }
